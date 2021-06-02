67884933f03bc46837c93369487f9f6352de24f0
Mohit Shah <mohitshah7777@gmail.com>
2021-06-02 10:25:35 +0530

    Added: Retrieve the Address Book data from the JSON server instead of local storage

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,6 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../pages/home.html",
-    add_person_page: "../pages/add-person.html"
+    add_person_page: "../pages/add-person.html",
+    server_url: "http://127.0.0.1:3000/AddressBookDB/"
 };
